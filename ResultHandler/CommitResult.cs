ef42c089f99cf1ff475c0fd076c4f0f2255a5540
Ahmed Abuelnour <Ahmed.Abuelnoor@outlook.com>
2022-03-09 09:46:36 +0200

    Add project files.

---
+++ ResultHandler/CommitResult.cs
@@ -0,0 +1,25 @@
+﻿namespace ResultHandler
+{
+    public class CommitResult
+    {
+        public string? ErrorMessage { get; set; }
+        public int? ErrorCode { get; set; }
+        public ResultType ResultType { get; set; }
+        public bool IsSuccess { get => !ErrorCode.HasValue; }
+    }
+    public class CommitResult<T> : CommitResult
+    {
+        public T? Value { get; set; }
+    }
+    public enum ResultType
+    {
+        Ok,
+        Invalid,
+        Unauthorized,
+        PartialOk,
+        NotFound,
+        PermissionDenied,
+        Unexpected,
+        Exception
+    }
+}
